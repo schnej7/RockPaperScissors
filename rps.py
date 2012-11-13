f8f230d1de8f038bf63b15fda3eb792c3e521e61
schnej7 <schnej7@rpi.edu>
2012-11-13 12:15:19 -0500

    made bayes class

--- rps.py
+++ rps.py
@@ -11,39 +11,44 @@ def beats( a ):
 choices = list(['R','P','S'] )
 depth = 3
 
-def record( cur ):
-    depth_end = len( in_hist ) - ( 1 )
-    depth_begin = len( in_hist ) - ( depth + 1 )
-    prev = str( in_hist[ depth_begin:depth_end ] ) + str( out_hist[ depth_begin:depth_end ] )
+class bayes:
+    def __init__( self ):
+        self.hist_probs = dict()
 
-    if prev not in hist_probs.keys():
-        hist_probs[prev] = dict()
-        hist_probs[prev][cur] = 1
-    elif cur not in hist_probs[prev].keys():
-        hist_probs[prev][cur] = 1
-    else:
-        hist_probs[prev][cur] += 1
+    def record( self, prev, cur ):
+        if prev not in self.hist_probs.keys():
+            self.hist_probs[prev] = dict()
+            self.hist_probs[prev][cur] = 1
+        elif cur not in self.hist_probs[prev].keys():
+            self.hist_probs[prev][cur] = 1
+        else:
+            self.hist_probs[prev][cur] += 1
 
-def predict( ):
-    cur_end = len( in_hist )
-    cur_begin = len( in_hist ) - ( depth )
-    cur = str( in_hist[ cur_begin:cur_end ] ) + str( out_hist[ cur_begin:cur_end ] )
-    if cur not in hist_probs.keys():
-        return random.choice( choices )
-    else:
-        return max(hist_probs[cur].iterkeys(), key=lambda k: hist_probs[cur][k])
+    def predict( self, prev ):
+        if cur not in self.hist_probs.keys():
+            return random.choice( choices )
+        else:
+            return max(self.hist_probs[cur].iterkeys(), key=lambda k: self.hist_probs[cur][k])
 
 
 if input == '':
+    bayes1 = bayes()
     turn = 0
-    hist_probs = dict()
     output = 'R'
     in_hist = list( ['x'] * depth )
     out_hist = list( ['x'] * (depth) ) + ['R']
 
 else:
+    depth_end = len( in_hist ) - ( 1 )
+    depth_begin = len( in_hist ) - ( depth + 1 )
+    prev = str( in_hist[ depth_begin:depth_end ] ) + str( out_hist[ depth_begin:depth_end ] )
+
+    cur_end = len( in_hist )
+    cur_begin = len( in_hist ) - ( depth )
+    cur = str( in_hist[ cur_begin:cur_end ] ) + str( out_hist[ cur_begin:cur_end ] )
+
     turn += 1
     in_hist += list( input )
-    record( input )
-    output = beats( predict() )
+    bayes1.record( prev, input )
+    output = beats( bayes1.predict( cur ) )
     out_hist += list( output )
